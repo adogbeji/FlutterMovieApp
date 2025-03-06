9facae65f3e53a5c514a1bf597d4abf1e4f1ab3e
adogbeji <baka_1993@yahoo.co.uk>
2025-03-06 14:43:42 +0000

    Add more styling for app theme

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -7,5 +7,22 @@ class AppTheme {
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.background,
     brightness: Brightness.dark,
+    snackBarTheme: const SnackBarThemeData(
+      backgroundColor: AppColors.background,
+      contentTextStyle: TextStyle(color: Colors.white),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.secondBackground,
+      hintStyle: const TextStyle(
+        color: Color(0xFFA7A7A7),
+        fontWeight: FontWeight.w400,
+      ),
+      contentPadding: const EdgeInsets.all(16),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide.none,
+      ),
+    ),
   );
 }
